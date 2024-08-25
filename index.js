29e1ef3d4ae5d82a4bbe7ae0e09d752036ca4c89
Caquinho Dev <166941412+CaquinhoDev@users.noreply.github.com>
2024-08-24 22:18:00 -0300

    mais comandos 🤑

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const {
   default: makeWASocket,
   useMultiFileAuthState,
+  DisconnectReason,
 } = require("@whiskeysockets/baileys");
 const { exec } = require("child_process");
+const axios = require("axios");
+const math = require("mathjs"); // Importando a biblioteca mathjs
 
 const PREFIX = "!";
+const SIMI_API_URL = "https://api.simsimi.vn/v1/simtalk"; // URL da API SimSimi
+
+let botStartTime = Date.now(); // Timestamp inicial para uptime
 
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("auth_info");
@@ -15,46 +21,43 @@ async function startBot() {
 
   sock.ev.on("creds.update", saveCreds);
 
+  sock.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+      console.log(
+        "Connection closed due to ",
+        lastDisconnect.error,
+        ", reconnecting ",
+        shouldReconnect
+      );
+      if (shouldReconnect) {
+        startBot();
+      }
+    } else if (connection === "open") {
+      console.log("Opened connection");
+    }
+  });
+
   sock.ev.on("messages.upsert", async ({ messages }) => {
     const msg = messages[0];
     if (!msg.message || msg.key.fromMe) return;
-
-    sock.ev.on("connection.update", (update) => {
-      const { connection, lastDisconnect } = update;
-      if (connection === "close") {
-        const shouldReconnect =
-          lastDisconnect.error?.output?.statusCode !==
-          DisconnectReason.loggedOut;
-        console.log(
-          "Connection closed due to ",
-          lastDisconnect.error,
-          ", reconnecting ",
-          shouldReconnect
-        );
-        // Reconnect if not logged out
-        if (shouldReconnect) {
-          startBot();
-        }
-      } else if (connection === "open") {
-        console.log("Opened connection");
-      }
-    });
 
     const text =
       msg.message.conversation || msg.message.extendedTextMessage?.text;
     if (!text || !text.startsWith(PREFIX)) return;
 
     const command = text.slice(PREFIX.length).trim().toLowerCase();
-
-    const time = msg.messageTimestamp;
-    const ms = Date.now() - time * 1000;
-
-    //COMANDOS
+    const timestampSent = Date.now(); // Timestamp do envio da mensagem
 
     // Comando de ping com reação
     if (command === "ping") {
+      const timestampReceived = Date.now(); // Timestamp do recebimento da resposta
+      const latency = timestampReceived / timestampSent; // Latência em ms
+
       await sock.sendMessage(msg.key.remoteJid, {
-        text: `Pong! 🏓\n\n⏳ Tempo de resposta do bot foi de *${ms}ms*.\n\n${getMessageEnd()}`,
+        text: `Pong! 🏓\n\n⏳ Tempo de resposta do bot foi de *${latency}ms*.\n\n${getMessageEnd()}`,
       });
       await sock.sendMessage(msg.key.remoteJid, {
         react: { text: "🏓", key: msg.key },
@@ -62,45 +65,36 @@ async function startBot() {
       return;
     }
 
-    // Comando de cálculo
-    if (command === "calcular") {
-      const [_, operation, num1, num2] = text.split(" ");
-      let result;
+    // Comando de cálculo usando mathjs
+    if (command.startsWith("calcular")) {
+      const expression = text.slice(PREFIX.length + 9).trim(); // Remove PREFIX e 'calcular'
 
-      switch (operation) {
-        case "soma":
-          result = parseFloat(num1) + parseFloat(num2);
-          break;
-        case "subtrai":
-          result = parseFloat(num1) - parseFloat(num2);
-          break;
-        case "multiplica":
-          result = parseFloat(num1) * parseFloat(num2);
-          break;
-        case "divide":
-          result = parseFloat(num1) / parseFloat(num2);
-          break;
-        default:
-          result = "Operação não reconhecida";
+      try {
+        const result = math.evaluate(expression);
+        await sock.sendMessage(msg.key.remoteJid, {
+          text: `Resultado: ${result}\n\n${getMessageEnd()}`,
+        });
+      } catch (error) {
+        await sock.sendMessage(msg.key.remoteJid, {
+          text: `Erro ao calcular a expressão: ${
+            error.message
+          }\n\n${getMessageEnd()}`,
+        });
       }
-
-      await sock.sendMessage(msg.key.remoteJid, {
-        text: `Resultado: ${result}\n${getMessageEnd()}`,
-      });
       return;
     }
 
     // Comando para abrir aplicativos no Windows
-    if (command === "abrir") {
-      const app = text.split(" ")[1];
+    if (command.startsWith("abrir")) {
+      const app = command.split(" ")[1];
       exec(app, (err) => {
         if (err) {
           sock.sendMessage(msg.key.remoteJid, {
-            text: `Erro ao abrir ${app}: ${err.message}\n${getMessageEnd()}`,
+            text: `Erro ao abrir ${app}: ${err.message}\n\n${getMessageEnd()}`,
           });
         } else {
           sock.sendMessage(msg.key.remoteJid, {
-            text: `${app} foi aberto com sucesso!\n${getMessageEnd()}`,
+            text: `${app} foi aberto com sucesso! 🎉\n\n${getMessageEnd()}`,
           });
         }
       });
@@ -114,11 +108,11 @@ async function startBot() {
           sock.sendMessage(msg.key.remoteJid, {
             text: `Erro ao desligar o computador: ${
               err.message
-            }\n${getMessageEnd()}`,
+            }\n\n${getMessageEnd()}`,
           });
         } else {
           sock.sendMessage(msg.key.remoteJid, {
-            text: "Computador será desligado!\n" + getMessageEnd(),
+            text: "Computador será desligado! 💻🔌\n\n" + getMessageEnd(),
           });
         }
       });
@@ -132,27 +126,102 @@ async function startBot() {
           sock.sendMessage(msg.key.remoteJid, {
             text: `Erro ao reiniciar o computador: ${
               err.message
-            }\n${getMessageEnd()}`,
+            }\n\n${getMessageEnd()}`,
           });
         } else {
           sock.sendMessage(msg.key.remoteJid, {
-            text: "Computador será reiniciado!\n" + getMessageEnd(),
+            text: "Computador será reiniciado! 🔄\n\n" + getMessageEnd(),
           });
         }
       });
       return;
     }
 
+    // Comando SimSimi
+    if (command.startsWith("simi")) {
+      const message = command.slice(4).trim();
+      if (!message) {
+        await sock.sendMessage(msg.key.remoteJid, {
+          text: `Por favor, forneça uma mensagem para o SimSimi.\n\n${getMessageEnd()}`,
+        });
+        return;
+      }
+
+      try {
+        const response = await axios.post(SIMI_API_URL, {
+          text: message,
+          lc: "pt", // Língua portuguesa
+        });
+
+        // Responde com a mensagem da API SimSimi, independentemente do status
+        const responseText =
+          response.data.success || "Resposta não encontrada.";
+
+        await sock.sendMessage(msg.key.remoteJid, {
+          text: responseText + `\n\n${getMessageEnd()}`,
+        });
+      } catch (error) {
+        await sock.sendMessage(msg.key.remoteJid, {
+          text: `Erro ao se comunicar com a API SimSimi: ${
+            error.message
+          }\n\n${getMessageEnd()}`,
+        });
+      }
+      return;
+    }
+
+    const userId = msg.key.participant || msg.key.remoteJid;
+    // Comando de menu
+    if (command === "menu") {
+      const menu = `༒W̷E̷L̷C̷O̷M̷E̷༒
+    『 𝐌𝐄𝐍𝐔 』
+  ╭════════════════════╯
+   | ೈ፝͜͡🤑 !calcular
+   | ೈ፝͜͡🤑 !simi 
+   | ೈ፝͜͡🤑 !desligar
+   | ೈ፝͜͡🤑 !reinciar
+   | ೈ፝͜͡🤑 !uptime
+   | ೈ፝͜͡🤑 !ping
+  ╰════════════════════╮
+  `;
+
+      await sock.sendMessage(msg.key.remoteJid, {
+        text: menu + `\n\n${getMessageEnd()}`,
+      });
+      return;
+    }
+
+    // Comando de uptime
+    if (command === "uptime") {
+      const uptime = formatUptime(Date.now() - botStartTime);
+      await sock.sendMessage(msg.key.remoteJid, {
+        text: `O bot está online há ${uptime}.\n\n${getMessageEnd()}`,
+      });
+      return;
+    }
+
     // Resposta padrão se o comando não for reconhecido
-    sock.sendMessage(msg.key.remoteJid, {
-      text: `Comando não reconhecido.\n${getMessageEnd()}`,
+    await sock.sendMessage(msg.key.remoteJid, {
+      text: `Comando não reconhecido. Tente novamente.\n\n${getMessageEnd()}`,
     });
   });
 }
 
+// Função para formatar o tempo de atividade
+function formatUptime(ms) {
+  const seconds = Math.floor(ms / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  return `*${days} dias, ${hours % 24} horas, ${minutes % 60} minutos e ${
+    seconds % 60
+  } segundos*`;
+}
+
 // Função para adicionar a mensagem personalizada no final de cada resposta
 function getMessageEnd() {
-  return "ミ★ MagoBot JS 1.0 ★彡";
+  return "ミ★ MagoBot JS 1.0 ★彡";
 }
 
 startBot();
